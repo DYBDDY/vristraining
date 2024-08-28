1fe2820bd202125991f7e51f7786d6f8827f05a3
DYBDDY <dannyhgc@gmail.com>
2024-08-28 14:28:52 +0800

    Replace existing files with new ones

--- aframe.js
+++ aframe.js
@@ -1,3 +1,24 @@
+// AFRAME.registerComponent('toggle-panel', {
+//   init: function () {
+//     var button = this.el;
+//     var panel = document.querySelector('#html-panel');
+//     // var panelContent = document.querySelector('#panel-content');
+
+//     button.addEventListener('click', function () {
+//       console.log("Button clicked");
+//       var isVisible = panel.getAttribute('visible') === true;
+
+//       if (isVisible) {
+//         panel.setAttribute('visible', false);
+//         // panelContent.style.display = 'none';
+//       } else {
+//         panel.setAttribute('visible', true);
+//         // panelContent.style.display = 'block';
+//       }
+//     });
+//   }
+// });
+
 AFRAME.registerComponent('toggle-panel', {
   init: function () {
     var button = this.el;
@@ -17,32 +38,19 @@ AFRAME.registerComponent('toggle-panel', {
 });
 
 
-// AFRAME.registerComponent('thumbstick-logging',{
+// AFRAME.registerComponent('thumbstick-logging', {
 //   init: function () {
-//     this.el.addEventListener('thumbstickmoved', this.logThumbstick);
+//     this.el.addEventListener('triggerdown', this.handleTriggerDown.bind(this));
 //   },
-//   logThumbstick: function (evt) {
-//     if (evt.detail.y > 0.95) { console.log("DOWN"); }
-//     if (evt.detail.y < -0.95) { console.log("UP"); }
-//     if (evt.detail.x < -0.95) { console.log("LEFT"); }
-//     if (evt.detail.x > 0.95) { console.log("RIGHT"); }
-//   }
+
+//   handleTriggerDown: function (evt) {
+//     var camera = document.querySelector('a-camera');
+//     var cursor = document.querySelector('a-cursor');
+
+//     var intersection = cursor.components.raycaster.getIntersection(this.el);
+
+//     if (intersection) {
+//       this.el.emit('click');
+//     }
+//   }  
 // });
-
-
-AFRAME.registerComponent('thumbstick-click', {
-  init: function () {
-    this.el.addEventListener('triggerdown', this.handleTriggerDown.bind(this));
-  },
-
-  handleTriggerDown: function (evt) {
-    var camera = document.querySelector('a-camera');
-    var cursor = document.querySelector('a-cursor');
-
-    var intersection = cursor.components.raycaster.getIntersection(this.el);
-
-    if (intersection) {
-      this.el.emit('click');
-    }
-  }  
-});
