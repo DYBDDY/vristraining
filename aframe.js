5170f46e3d01ba8f11e822c65134d3ab7cf565a8
DYBDDY <dannyhgc@gmail.com>
2024-08-28 15:04:53 +0800

    Replace existing files with new ones

--- aframe.js
+++ aframe.js
@@ -15,3 +15,23 @@ AFRAME.registerComponent('toggle-panel', {
     });
   }
 });
+
+
+AFRAME.registerComponent('thumbstick-movement', {
+  init: function () {
+    this.el.addEventListener('thumbstickmoved', this.handleThumbstick);
+  },
+  
+  handleThumbstick: function (evt) {
+    const speed = 0.1; 
+    
+    if (evt.detail.y > 0.95) { 
+      // 向後移動
+      this.el.object3D.position.x -= speed;
+    }
+    if (evt.detail.y < -0.95) {
+      // 向前移動 
+      this.el.object3D.position.x += speed;  
+    }
+  }
+});
