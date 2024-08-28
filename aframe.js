f3887e6c07276bfa5cd085295201e53074a658a3
DYBDDY <dannyhgc@gmail.com>
2024-08-28 13:57:09 +0800

    Replace existing files with new ones

--- aframe.js
+++ aframe.js
@@ -1,24 +1,3 @@
-// AFRAME.registerComponent('toggle-panel', {
-//   init: function () {
-//     var button = this.el;
-//     var panel = document.querySelector('#html-panel');
-//     // var panelContent = document.querySelector('#panel-content');
-
-//     button.addEventListener('click', function () {
-//       console.log("Button clicked");
-//       var isVisible = panel.getAttribute('visible') === true;
-
-//       if (isVisible) {
-//         panel.setAttribute('visible', false);
-//         // panelContent.style.display = 'none';
-//       } else {
-//         panel.setAttribute('visible', true);
-//         // panelContent.style.display = 'block';
-//       }
-//     });
-//   }
-// });
-
 AFRAME.registerComponent('toggle-panel', {
   init: function () {
     var button = this.el;
@@ -38,19 +17,3 @@ AFRAME.registerComponent('toggle-panel', {
 });
 
 
-// AFRAME.registerComponent('thumbstick-logging', {
-//   init: function () {
-//     this.el.addEventListener('triggerdown', this.handleTriggerDown.bind(this));
-//   },
-
-//   handleTriggerDown: function (evt) {
-//     var camera = document.querySelector('a-camera');
-//     var cursor = document.querySelector('a-cursor');
-
-//     var intersection = cursor.components.raycaster.getIntersection(this.el);
-
-//     if (intersection) {
-//       this.el.emit('click');
-//     }
-//   }  
-// });
